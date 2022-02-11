48e4629d6fadf765e21da6dcbf21bb5926155157
Luca Albinati <luca.albinati@gmail.com>
2022-02-11 16:19:36 +0100

    add script to create a zip of the source code

---
+++ create_zip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+WORDLE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $WORDLE_DIR
+
+function zip_source() {
+    ZIP_FILENAME="WordlHelper$1.zip"
+
+    zip -r $ZIP_FILENAME . > /dev/null
+
+    zip -d $ZIP_FILENAME "__MACOSX*" > /dev/null
+    zip -d $ZIP_FILENAME "*.DS_Store" > /dev/null
+
+    echo "Created '$ZIP_FILENAME'"
+}
+
+if [[ $# -eq 0 ]]; then
+    zip_source
+elif [[ $# -eq 1 ]]; then
+    if [[ "$1" == "--chrome" ]]; then
+        sed -i '' 's/"manifest_version": 2/"manifest_version": 3/g' manifest.json
+        zip_source "_Chrome"
+        sed -i '' 's/"manifest_version": 3/"manifest_version": 2/g' manifest.json
+    else
+        echo "Unknown argument: $1"
+    fi 
+else
+  echo "Unexpected number of arguments: expecting 0 or 1, instead got $#"
+fi
